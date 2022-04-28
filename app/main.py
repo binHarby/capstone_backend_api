e725add323a906cca9fe5e3e6bb6b821e792dcab
binHarby <lance.alameri@gmail.com>
2022-04-28 19:01:05 +0400

    some code

--- app/main.py
+++ app/main.py
@@ -4,9 +4,21 @@ from typing import List
 from . import schemas, utils, database
 from .routers import user,login,history
 from datetime import date,datetime
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 conn,cursor=database.run()
 app = FastAPI()
+# change allow_origins to allow certain domians and not others
+# NOTE: we are not talking to any website, but native apps, so you can keep origins empty 
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"]
+        )
 
 @app.get('/')
 
