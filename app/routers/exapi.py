ce4956b77f5e39626ef1f266e14a537d9b5b4a27
binHarby <lance.alameri@gmail.com>
2022-05-11 10:29:02 +0400

    edit image api lookup

--- app/routers/exapi.py
+++ app/routers/exapi.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, Response, status, HTTPException, APIRouter, Depends
 from typing import List
 from .. import schemas, utils, database,oauth
 from fastapi.responses import ORJSONResponse
+import ast
 conn,cursor=database.run()
 router = APIRouter(
         prefix='/Exapi',
@@ -16,3 +17,6 @@ def post_name(new_rest: schemas.ExapiNameReq, get_current_user: int = Depends(oa
 def post_upc(new_rest: schemas.ExapiUPCReq, get_current_user: int = Depends(oauth.get_current_user)):
     rest_info=utils.nutrix_upc(new_rest.upc)
     return ORJSONResponse(rest_info)
+@router.post("/image", status_code=status.HTTP_201_CREATED)
+def post_image(obj: dict,get_current_user: int = Depends(oauth.get_current_user)):
+    return obj 
